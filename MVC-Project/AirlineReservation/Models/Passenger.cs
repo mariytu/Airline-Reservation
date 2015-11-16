91220b7c9dd5139f7a072db4800a4725c8dbb940
Marianela <mari.ytu@gmail.com>
2015-11-16 15:26:07 -0300

    Model Passenger

--- MVC-Project/AirlineReservation/Models/Passenger.cs
+++ MVC-Project/AirlineReservation/Models/Passenger.cs
@@ -11,58 +11,78 @@ using System.Web;
 
 namespace AirlineReservation.Models
 {
-    public class Aircraft
+    public class Passenger
     {
         #region [Propiedades]
         public long ID { get; set; }
 
         [Required(ErrorMessage = "El campo nombre es requerido")]
         [DisplayName("Nombre")]
-        [StringLength(55)]
-        public string Name { get; set; }
+        [StringLength(30)]
+        public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "El campo capacidad es requerido")]
-        [DisplayName("Capacidad")]
-        public int Capacity { get; set; }
+        [DisplayName("Segundo Nombre")]
+        [StringLength(30)]
+        public string SecondName { get; set; }
 
-        [Required(ErrorMessage = "El campo código es requerido")]
-        [DisplayName("Código")]
-        [StringLength(3)]
-        public string Code { get; set; }
+        [Required(ErrorMessage = "El campo apellido es requerido")]
+        [DisplayName("Apellido")]
+        [StringLength(30)]
+        public string LastName { get; set; }
+
+        [DisplayName("Teléfono")]
+        public int PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "El campo dirección es requerido")]
+        [DisplayName("Dirección")]
+        [StringLength(40)]
+        public string AddressLine { get; set; }
+
+        [Required(ErrorMessage = "El campo correo es requerido")]
+        [DisplayName("Correo")]
+        [StringLength(45)]
+        public string EmailAddress { get; set; }
+
+        [DisplayName("Dirección")]
+        public long CityID { get; set; }
         #endregion
 
         #region [Metodos]
         /// <summary>
-        /// Establece las propiedades del Aircraft dado un NpgsqlDataReader
+        /// Establece las propiedades del passenger dado un NpgsqlDataReader
         /// (miturriaga)
         /// </summary>
-        /// <param name="dr">El NpgsqlDataReader que contiene los datos del aircraft</param>
+        /// <param name="dr">El NpgsqlDataReader que contiene los datos del passenger</param>
         public void SetDesde(NpgsqlDataReader dr)
         {
-            this.ID = Convert.ToInt64(dr["aircraftID"]);
-            this.Name = dr["aircraftName"].ToString();
-            this.Capacity = Convert.ToInt32(dr["aircraftCapacity"]);
-            this.Code = dr["aircraftCode"].ToString();
+            this.ID = Convert.ToInt64(dr["passengerID"]);
+            this.FirstName = dr["firstName"].ToString();
+            this.SecondName = dr["secondName"].ToString();
+            this.LastName = dr["lastName"].ToString();
+            this.PhoneNumber = Convert.ToInt32(dr["phoneNumber"]);
+            this.AddressLine = dr["addressLine"].ToString();
+            this.EmailAddress = dr["emailAddress"].ToString();
+            this.CityID = Convert.ToInt64(dr["cityID"]);
         }
 
         /// <summary>
-        /// Selecciona los aircraft dada la pagina y cantidad de resultados por pagina
+        /// Selecciona los passenger dada la pagina y cantidad de resultados por pagina
         /// (miturriaga)
         /// </summary>
         /// <param name="cantidadResultados">Cantidad de resultado por pagina</param>
         /// <param name="pagina">Pagina que se necesita ver</param>
-        /// <returns>La lista de aircraft</returns>
-        public List<Aircraft> Todos(int cantidadResultados, int pagina)
+        /// <returns>La lista de passenger</returns>
+        public List<Passenger> Todos(int cantidadResultados, int pagina)
         {
 
-            var aircrafts = new List<Aircraft>();
+            var passengers = new List<Passenger>();
             int index = cantidadResultados * (pagina - 1);
 
             try
             {
                 string connString = ConfigurationManager.ConnectionStrings["PostgresConnection"].ConnectionString;
 
-                var comando = new NpgsqlCommand() { CommandText = "Aircraft_Todos", CommandType = CommandType.StoredProcedure };
+                var comando = new NpgsqlCommand() { CommandText = "Passenger_Todos", CommandType = CommandType.StoredProcedure };
                 comando.Parameters.Add(new NpgsqlParameter("inIndex", NpgsqlDbType.Integer));
                 comando.Parameters[0].Value = index;
                 comando.Parameters.Add(new NpgsqlParameter("inNext", NpgsqlDbType.Integer));
@@ -76,31 +96,31 @@ namespace AirlineReservation.Models
 
                     while (ds.Read())
                     {
-                        var aircraft = new Aircraft();
-                        aircraft.SetDesde(ds);
-                        aircrafts.Add(aircraft);
+                        var passenger = new Passenger();
+                        passenger.SetDesde(ds);
+                        passengers.Add(passenger);
                     }
                     conn.Close();
                 }
             }
             catch (Exception ex) { }
 
-            return aircrafts;
+            return passengers;
         }
 
         /// <summary>
-        /// Selecciona un aircraft dado el id que se entrega como parametro 
+        /// Selecciona un passenger dado el id que se entrega como parametro 
         /// (miturriaga)
         /// </summary>
-        /// <param name="id">el id del aircraft a seleccionar</param>
-        /// <returns>True si el aircraft fue encontrado, false en caso contrario</returns>
+        /// <param name="id">el id del passenger a seleccionar</param>
+        /// <returns>True si el passenger fue encontrado, false en caso contrario</returns>
         public bool Seleccionar(long id)
         {
             try
             {
                 string connString = ConfigurationManager.ConnectionStrings["PostgresConnection"].ConnectionString;
 
-                var comando = new NpgsqlCommand() { CommandText = "Aircraft_Seleccionar", CommandType = CommandType.StoredProcedure };
+                var comando = new NpgsqlCommand() { CommandText = "Passenger_Seleccionar", CommandType = CommandType.StoredProcedure };
                 comando.Parameters.Add(new NpgsqlParameter("inID", NpgsqlDbType.Integer));
                 comando.Parameters[0].Value = id;
 
@@ -125,7 +145,7 @@ namespace AirlineReservation.Models
         }
 
         /// <summary>
-        /// Crea este nuevo aircrat en la base de datos
+        /// Crea este nuevo passenger en la base de datos
         /// (miturriaga)
         /// </summary>
         public void Crear()
@@ -134,16 +154,24 @@ namespace AirlineReservation.Models
             {
                 string connString = ConfigurationManager.ConnectionStrings["PostgresConnection"].ConnectionString;
 
-                var comando = new NpgsqlCommand() { CommandText = "Aircraft_Crear", CommandType = CommandType.StoredProcedure };
+                var comando = new NpgsqlCommand() { CommandText = "Passenger_Crear", CommandType = CommandType.StoredProcedure };
                 comando.Parameters.Add(new NpgsqlParameter("outID", NpgsqlDbType.Integer));
                 comando.Parameters[0].Direction = ParameterDirection.Output;
                 comando.Parameters[0].Value = 0;
-                comando.Parameters.Add(new NpgsqlParameter("inName", NpgsqlDbType.Text));
-                comando.Parameters[1].Value = this.Name;
-                comando.Parameters.Add(new NpgsqlParameter("inCapacity", NpgsqlDbType.Integer));
-                comando.Parameters[2].Value = this.Capacity;
-                comando.Parameters.Add(new NpgsqlParameter("inCode", NpgsqlDbType.Text));
-                comando.Parameters[3].Value = this.Code;
+                comando.Parameters.Add(new NpgsqlParameter("inFirstName", NpgsqlDbType.Text));
+                comando.Parameters[1].Value = this.FirstName;
+                comando.Parameters.Add(new NpgsqlParameter("inSecondName", NpgsqlDbType.Text));
+                comando.Parameters[2].Value = this.SecondName;
+                comando.Parameters.Add(new NpgsqlParameter("inLastName", NpgsqlDbType.Text));
+                comando.Parameters[3].Value = this.LastName;
+                comando.Parameters.Add(new NpgsqlParameter("inPhoneNumber", NpgsqlDbType.Integer));
+                comando.Parameters[4].Value = this.PhoneNumber;
+                comando.Parameters.Add(new NpgsqlParameter("inAddressLine", NpgsqlDbType.Text));
+                comando.Parameters[5].Value = this.AddressLine;
+                comando.Parameters.Add(new NpgsqlParameter("inEmailAddress", NpgsqlDbType.Text));
+                comando.Parameters[6].Value = this.EmailAddress;
+                comando.Parameters.Add(new NpgsqlParameter("inCityID", NpgsqlDbType.Integer));
+                comando.Parameters[7].Value = this.CityID;
 
                 using (var conn = new NpgsqlConnection(connString))
                 {
@@ -160,7 +188,7 @@ namespace AirlineReservation.Models
         }
 
         /// <summary>
-        /// Modifica este aircraft en la base de datos
+        /// Modifica este passenger en la base de datos
         /// (miturriaga)
         /// </summary>
         public void Modificar()
@@ -169,15 +197,23 @@ namespace AirlineReservation.Models
             {
                 string connString = ConfigurationManager.ConnectionStrings["PostgresConnection"].ConnectionString;
 
-                var comando = new NpgsqlCommand() { CommandText = "Aircraft_Modificar", CommandType = CommandType.StoredProcedure };
+                var comando = new NpgsqlCommand() { CommandText = "Passenger_Modificar", CommandType = CommandType.StoredProcedure };
                 comando.Parameters.Add(new NpgsqlParameter("inID", NpgsqlDbType.Integer));
                 comando.Parameters[0].Value = this.ID;
-                comando.Parameters.Add(new NpgsqlParameter("inName", NpgsqlDbType.Text));
-                comando.Parameters[1].Value = this.Name;
-                comando.Parameters.Add(new NpgsqlParameter("inCapacity", NpgsqlDbType.Integer));
-                comando.Parameters[2].Value = this.Capacity;
-                comando.Parameters.Add(new NpgsqlParameter("inCode", NpgsqlDbType.Text));
-                comando.Parameters[3].Value = this.Code;
+                comando.Parameters.Add(new NpgsqlParameter("inFirstName", NpgsqlDbType.Text));
+                comando.Parameters[1].Value = this.FirstName;
+                comando.Parameters.Add(new NpgsqlParameter("inSecondName", NpgsqlDbType.Text));
+                comando.Parameters[2].Value = this.SecondName;
+                comando.Parameters.Add(new NpgsqlParameter("inLastName", NpgsqlDbType.Text));
+                comando.Parameters[3].Value = this.LastName;
+                comando.Parameters.Add(new NpgsqlParameter("inPhoneNumber", NpgsqlDbType.Integer));
+                comando.Parameters[4].Value = this.PhoneNumber;
+                comando.Parameters.Add(new NpgsqlParameter("inAddressLine", NpgsqlDbType.Text));
+                comando.Parameters[5].Value = this.AddressLine;
+                comando.Parameters.Add(new NpgsqlParameter("inEmailAddress", NpgsqlDbType.Text));
+                comando.Parameters[6].Value = this.EmailAddress;
+                comando.Parameters.Add(new NpgsqlParameter("inCityID", NpgsqlDbType.Integer));
+                comando.Parameters[7].Value = this.CityID;
 
                 using (var conn = new NpgsqlConnection(connString))
                 {
@@ -191,7 +227,7 @@ namespace AirlineReservation.Models
         }
 
         /// <summary>
-        /// Elimina este aircraft de la base de datos
+        /// Elimina este passenger de la base de datos
         /// (miturriaga)
         /// </summary>
         public void Eliminar()
@@ -200,7 +236,7 @@ namespace AirlineReservation.Models
             {
                 string connString = ConfigurationManager.ConnectionStrings["PostgresConnection"].ConnectionString;
 
-                var comando = new NpgsqlCommand() { CommandText = "Aircraft_Eliminar", CommandType = CommandType.StoredProcedure };
+                var comando = new NpgsqlCommand() { CommandText = "Passenger_Eliminar", CommandType = CommandType.StoredProcedure };
                 comando.Parameters.Add(new NpgsqlParameter("inID", NpgsqlDbType.Integer));
                 comando.Parameters[0].Value = this.ID;
 
@@ -219,10 +255,10 @@ namespace AirlineReservation.Models
         /// Retorna un string que representa brevemente esta entidad
         /// (miturriaga)
         /// </summary>
-        /// <returns>Un string con la representacion del aircraft</returns>
+        /// <returns>Un string con la representacion del passenger</returns>
         public override string ToString()
         {
-            return string.Format("Aircraft (ID: {0}, Name: {1})", this.ID, this.Name);
+            return string.Format("Passenger (ID: {0}, Name: {1}, LastName: {2})", this.ID, this.FirstName, this.LastName);
         }
 
         #endregion
