dcfbc16251ca720a8db05b59b5b5bc569623597e
Liliana Barros <lilianabarrosc@gmail.com>
2015-11-18 19:32:29 -0300

    PS 318 completado :)

--- SQL/318_ItineraryReservation_CheckIn.sql
+++ SQL/318_ItineraryReservation_CheckIn.sql
@@ -6,19 +6,28 @@ DROP FUNCTION IF EXISTS ItineraryReservation_CheckIn () $$
 -- Descripcion	: Realiza el check-in de una reserva de vuelo
 -- =============================================
 
-CREATE OR REPLACE FUNCTION ItineraryReservation_CheckIn (
-	IN inID integer
-)
+CREATE OR REPLACE FUNCTION ItineraryReservation_CheckIn ( inID integer)
 RETURNS void AS
-$BODY$ BEGIN
 
-	-- IMPLEMENTACION DEL SP INCLUIR EXCEPTION
-
---EXCEPTION
-
---WHEN SQLSTATE '23503' THEN  
-	--RAISE EXCEPTION 'No se pudo borrar el  avion, ya que tiene vuelos asociados a el ';
-	
+$BODY$BEGIN
+		IF (SELECT COUNT(*) 
+			FROM "ItineraryReservation"
+			LEFT JOIN "ReservationState" ON "ReservationState"."reservationID" = "ItineraryReservation"."reservationState"
+			WHERE   "ItineraryReservation"."reservationID" = inID AND
+					"ReservationState"."reservationName" = 'Reserved'
+			) = 1 THEN
+		    IF (SELECT  "Payment"."paymentAmount"
+				FROM "ItineraryReservation"
+				LEFT JOIN "Payment" ON "Payment"."paymentID" = "ItineraryReservation"."paymentID"
+				WHERE   "ItineraryReservation"."reservationID" = inID
+			) > 0 THEN
+		    	RAISE 'Check-in realizado exitosamente';
+			ELSE
+				RAISE 'No se pudo realizar Check-in, no ha pagado su Itinerary Reservation';
+			END IF;
+		ELSE
+			RAISE 'Ya se realizo un Check-in';
+		END IF;
 END;
 $BODY$ LANGUAGE 'plpgsql' VOLATILE
 COST 100 $$
