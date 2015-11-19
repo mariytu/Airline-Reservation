834140df7610905a1ace666dcf33fea355582029
Marianela <mari.ytu@gmail.com>
2015-11-19 02:37:04 -0300

    Agrega modelo de reservacion y metodos aeropuertos y pasajeros todos

---
+++ MVC-Project/AirlineReservation/Models/Reservacion.cs
@@ -0,0 +1,77 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace AirlineReservation.Models
+{
+    public class Reservacion
+    {
+        #region [Propiedades]
+
+        [Required(ErrorMessage = "El campo agencia es requerido")]
+        [DisplayName("Agencia")]
+        public long AgenciaID { get; set; }
+
+        [Required(ErrorMessage = "El campo aeropuerto de origen es requerido")]
+        [DisplayName("Aeropuerto de Origen")]
+        public long AeropuertoOrigenID { get; set; }
+
+        [Required(ErrorMessage = "El campo aeropuerto de destino es requerido")]
+        [DisplayName("Aeropuerto de Destino")]
+        public long AeropuertoDestinoID { get; set; }
+
+        [Required(ErrorMessage = "El campo pasajero es requerido")]
+        [DisplayName("Pasajero")]
+        public long PasajeroID { get; set; }
+
+        #endregion
+
+        #region [Metodos]
+
+        public static List<Aircraft> AeropuertosTodos()
+        {
+            return Aircraft.Todos();
+        }
+
+        public List<Passenger> PasajerosTodos()
+        {
+
+            var passengers = new List<Passenger>();
+
+            try
+            {
+                string connString = ConfigurationManager.ConnectionStrings["PostgresConnection"].ConnectionString;
+
+                var comando = new NpgsqlCommand()
+                {
+                    CommandText = "SELECT * FROM \"Passengers\""
+                };
+
+                using (var conn = new NpgsqlConnection(connString))
+                {
+                    conn.Open();
+                    comando.Connection = conn;
+                    NpgsqlDataReader ds = comando.ExecuteReader();
+
+                    while (ds.Read())
+                    {
+                        var passenger = new Passenger();
+                        passenger.SetDesde(ds);
+                        passengers.Add(passenger);
+                    }
+                    conn.Close();
+                }
+            }
+            catch (Exception ex) { }
+
+            return passengers;
+        }
+        #endregion
+    }
+}
