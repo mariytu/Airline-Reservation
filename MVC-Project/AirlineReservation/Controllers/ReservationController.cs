ef246aa7b0d0c223fde1db70cd4b1dfc8465f681
Sebastian Felipe Diaz Avila <diazavilasebastian@gmail.com>
2015-11-19 02:26:45 -0300

    algunas agregaciones para poder crear una reserva

---
+++ MVC-Project/AirlineReservation/Controllers/ReservationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AirlineReservation.Controllers
+{
+    public class ReservationController : Controller
+    {
+        // GET: Reservation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+    }
+}
