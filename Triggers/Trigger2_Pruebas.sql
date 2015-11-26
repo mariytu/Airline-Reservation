1f1c0edfbc10cb23627809bb05b2f8f4a5901191
lilianabarrosc <lilianabarrosc@gmail.com>
2015-11-26 00:25:23 -0300

    cambios en las consultas

--- Triggers/Trigger2_Pruebas.sql
+++ Triggers/Trigger2_Pruebas.sql
@@ -1,12 +1,20 @@
 -- Consultas de prueba
 -- No se puede realizar un pago trascurridos 3 dias desde que se creo la reserva
-INSERT INTO "Payment"("paymentDate", "paymentAmount", "paymentTypeID")
-VALUES ((SELECT now()), 100, 2);
+UPDATE "Payment"
+SET "paymentAmount"= 300
+WHERE "Payment"."paymentID" = ID_INSTANCE;
 
+-- Consulta de actualización del pago con fecha
 UPDATE "Payment"
 SET "paymentDate"= (SELECT now()) ,"paymentAmount"= 300
 WHERE "Payment"."paymentID" = ID_INSTANCE;
 
+-- Consulta para verificar la actualización del pago
 SELECT *
 FROM "Payment"
 WHERE "Payment"."paymentID" = ID_INSTANCE;
+
+--Consulta para validar fechas
+SELECT "Payment"."paymentDate", "ItineraryReservation"."dateReservationMade"
+FROM "Payment", "ItineraryReservation"
+WHERE "Payment"."paymentID" = ID_INSTANCE AND "ItineraryReservation"."paymentID" = ID_INSTANCE;
